f99bb7054447e061eb61d1504fdb402a3e75d2e4
Ravi Samatham <ravikantsv@yahoo.com>
2016-11-08 15:41:20 -0800

    changed range of the guess number

--- NumberGuessGame/app/src/main/java/com/samatham/numberguessgame/GameScreen1.java
+++ NumberGuessGame/app/src/main/java/com/samatham/numberguessgame/GameScreen1.java
@@ -13,34 +13,40 @@ public class GameScreen1 extends AppCompatActivity {
     private EditText editor;
     private TextView msgText;
     private int numGuess;
+    public static final int ST_NUM = 1;
+    public static final int LAST_NUM = 32;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen1);
         Random r = new Random();
-        numGuess = (r.nextInt(10 - 1) + 1);
+        numGuess = (r.nextInt(LAST_NUM - ST_NUM) + ST_NUM);
+        msgText = (TextView) findViewById(R.id.tv_feedback);
+        String msgDef = "Enter a Number between " + ST_NUM + " to " + LAST_NUM;
+        msgText.setText(msgDef);
     }
 
     public void compareNum(View view) {
         editor = (EditText) findViewById(R.id.guessNum);
-
+        msgText = (TextView) findViewById(R.id.tv_feedback);
+//        String msgDef = "Enter a Number between " + ST_NUM + " to " + LAST_NUM;
+//        msgText.setText(msgDef);
         int inputNum = Integer.valueOf(editor.getText().toString());
-        String msgText2 = "System Guessed: " + String.valueOf(numGuess) + "\n";
-        String msgText3 = "You Entered: " + String.valueOf(inputNum) + "\n";
+//        String msgText2 = "System Guessed: " + String.valueOf(numGuess) + "\n";
+//        String msgText3 = "You Entered: " + String.valueOf(inputNum) + "\n";
 
         String msgText1;
         if(numGuess > inputNum){
-            msgText1 = "You guessed a smaller number";
+            msgText1 = "HIGHER";
         }else if(numGuess < inputNum){
-            msgText1 = "You guessed a larger number";
+            msgText1 = "SMALLER";
         }else {
             msgText1 = "CONGRATULATIONS - YOU ARE RIGHT";
             Intent intent = new Intent(GameScreen1.this, congratulation.class);
             startActivity(intent);
         }
+//        msgText.setText(msgText1 + "\n" + msgText2 + msgText3);
+        msgText.setText(msgText1);
 
-
-        msgText = (TextView) findViewById(R.id.tv_feedback);
-        msgText.setText(msgText1 + "\n" + msgText2 + msgText3);
     }
 }
