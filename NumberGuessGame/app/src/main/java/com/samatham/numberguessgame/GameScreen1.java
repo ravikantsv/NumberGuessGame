d091d562e432db5659e49650b9b0345888a7646e
Ravi Samatham <ravikantsv@yahoo.com>
2016-11-08 13:19:15 -0800

    added method to respond to Guess button-Next add logic

--- NumberGuessGame/app/src/main/java/com/samatham/numberguessgame/GameScreen1.java
+++ NumberGuessGame/app/src/main/java/com/samatham/numberguessgame/GameScreen1.java
@@ -2,12 +2,26 @@ package com.samatham.numberguessgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GameScreen1 extends AppCompatActivity {
-
+    private EditText editor;
+    private TextView msgText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen1);
+    }
+
+    public void compareNum(View view) {
+        editor = (EditText) findViewById(R.id.guessNum);
+
+        int inputNum = Integer.valueOf(editor.getText().toString());
+        String msgText1 = "You entered: " + String.valueOf(inputNum);
+
+        msgText = (TextView) findViewById(R.id.tv_feedback);
+        msgText.setText(msgText1);
     }
 }
