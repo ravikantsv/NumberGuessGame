8e494248b97905a094ba4a19ddb8e43e947c4855
Ravi Samatham <ravikantsv@yahoo.com>
2016-11-08 13:40:04 -0800

    code to get a random number, compare with guess and display message

--- NumberGuessGame/app/src/main/java/com/samatham/numberguessgame/GameScreen1.java
+++ NumberGuessGame/app/src/main/java/com/samatham/numberguessgame/GameScreen1.java
@@ -6,22 +6,38 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class GameScreen1 extends AppCompatActivity {
     private EditText editor;
     private TextView msgText;
+    private int numGuess;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen1);
+        Random r = new Random();
+        numGuess = (r.nextInt(10 - 1) + 1);
     }
 
     public void compareNum(View view) {
         editor = (EditText) findViewById(R.id.guessNum);
 
         int inputNum = Integer.valueOf(editor.getText().toString());
-        String msgText1 = "You entered: " + String.valueOf(inputNum);
+        String msgText2 = "System Guessed: " + String.valueOf(numGuess) + "\n";
+        String msgText3 = "You Entered: " + String.valueOf(inputNum) + "\n";
+
+        String msgText1;
+        if(numGuess > inputNum){
+            msgText1 = "You guessed a smaller number";
+        }else if(numGuess < inputNum){
+            msgText1 = "You guessed a larger number";
+        }else {
+            msgText1 = "CONGRATULATIONS - YOU ARE RIGHT";
+        }
+
 
         msgText = (TextView) findViewById(R.id.tv_feedback);
-        msgText.setText(msgText1);
+        msgText.setText(msgText1 + "\n" + msgText2 + msgText3);
     }
 }
